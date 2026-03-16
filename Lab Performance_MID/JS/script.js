a083d9ec349d0bf24c0719a9ce54d58c1a3592d7
abidulislamaiub <abidjihan1123@gmail.com>
2026-03-16 23:03:48 +0600

    done

--- Lab Performance_MID/JS/script.js
+++ Lab Performance_MID/JS/script.js
@@ -1,27 +1,25 @@
+
 const unitPrice = 1000;
 const days = 30;
 
-const quantityInput = document.getElementById("quantity");
+const quantityInput = document.getElementById("quantity_per_day");
 const totalPriceInput = document.getElementById("totalPrice");
 
 function calculateTotal(){
 
-let quantity = parseInt(quantityInput.value) || 0;
+    let quantity_per_day = parseInt(quantityInput.value) || 0;
 
-if(quantity < 0){
-alert("Quantity cannot be negative. Resetting to 0.");
-quantity = 0;
-quantityInput.value = 0;
+    if(quantity_per_day < 0){
+        alert("Quantity cannot be negative. Resetting to 0.");
+        quantity_per_day = 0;
+        quantityInput.value = 0;
+    }
+
+    let totalPrice = unitPrice * quantity_per_day * days;
+    totalPriceInput.value = totalPrice;
+
+    if(totalPrice > 1000){
+        alert("Congratulations! You are eligible for a gift coupon.");
+    }
 }
-
-let total = unitPrice * quantity * days;
-
-totalPriceInput.value = total;
-
-if(total > 1000){
-alert("Congratulations! You are eligible for a gift coupon.");
-}
-
-}
-
 quantityInput.addEventListener("input", calculateTotal);
